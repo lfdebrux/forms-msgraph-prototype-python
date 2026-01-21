7397a616ed30583003e7f3e3bb9f290728744016
Laurence de Bruxelles <laurence.debruxelles@dsit.gov.uk>
2026-01-21 10:12:24 +0200

    Create Excel file in app folder

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask
+from flask import Flask, render_template_string, request
 
 from identity.flask import Auth
 
@@ -44,27 +44,80 @@ def authenticated(*, context):
             Email: {context["user"]["preferred_username"]}
         </p>
 
-        <form action="/create" method="post">
-            <button>Create an Excel spreadsheet in the application folder</button>
-        </form>
+        <a href="/new">Create an Excel spreadsheet</a>
     """
 
-@app.route("/create", methods=["GET", "POST"])
+@app.route("/new")
 @auth.login_required(scopes=scopes)
-def create(*, context):
-    drives = requests.get(
+def new(*, context):
+    response = requests.get(
         "https://graph.microsoft.com/v1.0/me/drives",
         headers={"Authorization": f"Bearer {context['access_token']}"},
         timeout=30,
     )
+    response.raise_for_status()
 
-    print(drives.json())
+    drives = response.json()
+    print({"drives": drives})
+
+    # NOTE: we probably want to filter out OneDrive and ODCMetadataArchive
+    # NOTE: might be better to look at sites rather than drives?
+    drive_ids = {
+        drive['name']: drive['id']
+        for drive in drives['value']
+    }
+
+    return render_template_string("""
+        <h1>Choose a drive to store the spreadsheet in</h1>
+
+        <form action="/create" method="post">
+            <fieldset>
+                {% for drive_name, drive_id in drive_ids.items() %}
+                <div>
+                    <input type="radio" id="{{ drive_id }}" name="drive" value="{{ drive_id }}" />
+                    <label for="{{ drive_id }}">{{ drive_name }}</label>
+                </div>
+                {% endfor %}
+            </fieldset>
+
+            <button>Create Excel spreadsheet here</button>
+        </form>
+    """, drive_ids=drive_ids)
+
+@app.post("/create")
+@auth.login_required(scopes=scopes)
+def create(*, context):
+    drive_id = request.form['drive']
+
+    if not drive_id:
+        return redirect("/new")
 
     response = requests.get(
-        "https://graph.microsoft.com/v1.0/me/drive/special/approot",
+        f"https://graph.microsoft.com/v1.0/drives/{drive_id}/special/approot",
         headers={"Authorization": f"Bearer {context['access_token']}"},
         timeout=30
     )
+    response.raise_for_status()
+
+    approot = response.json()
+    print({"approot": approot})
+
+    approot_item_id = approot["id"]
+    form_name = "Test form"
+    file_name = f"{ form_name }.xlsx"
+
+    # This is documented in https://learn.microsoft.com/en-us/answers/questions/830336/is-there-any-ms-graph-api-to-create-workbook-in-gi#answer-1348868 (and nowhere else?)
+    response = requests.post(
+        f"https://graph.microsoft.com/v1.0/drives/{drive_id}/items/{approot_item_id}/children",
+        headers={"Authorization": f"Bearer {context['access_token']}"},
+        timeout=30,
+        json={
+            "name": file_name,
+            "file": { },
+            "@microsoft.graph.conflictBehavior": "rename",
+        }
+    )
+    response.raise_for_status()
 
     print(response.json())
 
